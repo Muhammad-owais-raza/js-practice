dc52bcfdf76a9052e131c9b380ef860235fae6e4
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-06-16 01:15:48 +0500

    js intro practice

---
+++ 01-js-intro/Ecmascript.js
@@ -0,0 +1,24 @@
+// alert("Salam");
+
+// prompt("Enter your name"); // prompt user sa info lanay k liya use hota ha
+
+
+console.log("Salam"); // is sa hum ksi bhi chez ko console ma print kartay ha
+
+console.log(12 + 12); 
+
+// variable name      variable value
+let myName = "Muhammad Owais Raza"; // This is variable
+
+
+console.log(myName);
+console.log(myName);
+console.log(myName);
+
+
+
+let userName= prompt("Enter your name"); 
+
+alert("Welcom " + userName);
+
+

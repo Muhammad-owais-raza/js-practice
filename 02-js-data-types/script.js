7e2155e321bacd159ed6e383a7642fb69a3e7b50
Muhammad-owais-raza <owaisraza.codes@gmail.com>
2026-06-25 05:50:06 +0500

    js data types larning

---
+++ 02-js-data-types/script.js
@@ -0,0 +1,36 @@
+ // 1) Primitive
+
+ // 7 types : Boolean , Bigint , String , Symbole , Number , Null , Undefined
+ 
+ // in ko yad karnay ka esy tarika " BBSSNNU "
+
+
+
+ // 2) Reference ( Non primitive )
+
+ // Array , Objects , Functions 
+
+ 
+
+ // Examples to all data types :
+
+
+ let num = 5847348934; // Number
+
+ let text = "hello";   // String
+
+ let isPresent = true; // Boolean (han ya na)
+
+ let b = undefined;    // Undefined
+
+ let courseResult = null; // Null (empty variable)
+
+ let s = Symbol("hello");  // Symbole
+
+
+ let b = 485343849343843488457454n; // Bigint "is ko is tarikay sa bhi likh saktay ha"
+
+ let b = BigInt(485343849343843488457454); // Bigint "or is ko is tarikay sa bhi likh saktay ha"
+
+ console.log(typeof isPresent); /* typeof "ya founction kisi ki bhi data type 
+                                malom karnay k liya use hota ha" */
